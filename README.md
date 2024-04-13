07c330298811de05b04b5fa3905d4d092bae59b2
LuoXianchao <nhjclxc@163.com>
2024-04-13 19:56:38 +0800

    初始提交

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jenkins-test
+
 Jenkins测试仓库
---
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LuoXianchao
+ * @since 2024/04/13 19:54
+ */
+@RestController
+@RequestMapping
+public class TestController {
+    @GetMapping("/test1")
+    public Object test1(){
+        System.out.println("test1被请求");
+        return "test1    19.56";
+    }
+}
