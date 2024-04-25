398c69e8e40e320be4a6d6056f681e5c7b085d60
LuoXianchao <nhjclxc@163.com>
2024-04-25 20:40:14 +0800

    更新1

--- src/main/java/com/example/jenkinstest/controller/TestController.java
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.jenkinstest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,18 @@ import java.time.LocalDateTime;
  * @since 2024/04/13 19:54
  */
 @RestController
-@RequestMapping
+@RequestMapping("/testCon")
 public class TestController {
+
+    @Value("${server.port}")
+    private String port;
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/test1")
     public Object test1(){
-        String str = "test 999   " + LocalDateTime.now();
-        System.out.println("test1被请求, " + str);
+        String str = "test1被请求,  " + LocalDateTime.now() + ",, port=" + port + ", appName = " + appName;
+        System.out.println(str);
         return str;
     }
 }
